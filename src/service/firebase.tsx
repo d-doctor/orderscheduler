502a5b92c2e9c6b970ed6462a4ea84b36d71c7bb
Dustin Falkenberg <dustin.falkenberg@gmail.com>
2023-11-05 22:59:23 -0600

    chore: more to the other firebase app

--- src/service/firebase.tsx
+++ src/service/firebase.tsx
@@ -2,11 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBx70bRRdyez9Zd9VW_lGBkKm9jnIwt_6U",
-  authDomain: "se-job-scheduler.firebaseapp.com",
-  //   authDomain: "se-job-scheduler.web.app",
-  projectId: "se-job-scheduler",
-  appId: "",
+  apiKey: "AIzaSyDdXPJSEX91iTv0Sn5yaTDhOC_-DAhlbY4",
+  authDomain: "se-data-job-scheduler.firebaseapp.com",
+  projectId: "se-data-job-scheduler",
+  storageBucket: "se-data-job-scheduler.appspot.com",
+  messagingSenderId: "973241811935",
+  appId: "1:973241811935:web:87c9da75157e61e914ba6a",
+  measurementId: "G-SVTN8EPQPC",
 };
 
 const firebaseapp = initializeApp(firebaseConfig);
